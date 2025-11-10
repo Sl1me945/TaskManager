fa75098042005dc31cd9486a2906ab0eb5141f3a
Максим Скрипник <4elokek4elovek@gmail.com>
2025-11-10 22:58:04 +0200

    feat: add interface ITaskRepository

---
+++ ToDoApp/Application/Interfaces/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using ToDoApp.Domain.Entities.Tasks;
+
+namespace ToDoApp.Application.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<IReadOnlyList<BaseTask>> GetByUserIdAsync(Guid userId);
+        Task<BaseTask?> GetByIdAsync(Guid id);
+        Task AddAsync(BaseTask task);
+        Task UpdateAsync(BaseTask task);
+        Task DeleteAsync(Guid id);
+    }
+}
